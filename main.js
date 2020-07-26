31e978831859d17886d1ec7e9aa71732b362f81b
Dent24 <cljhs90607@gmail.com>
2020-07-26 11:32:11 +0800

    存點1

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("確認連接");
+
+var vue1 = new Vue({
+    el: "#vue1",
+    data: {
+        text: "Hi,I am VUE",
+        a: 999,
+        b: 3,
+    },
+});
+
+var vue2 = new Vue({
+    el: "#vue2",
+    data: {
+        aligment: "center",
+        yahoo: "https://tw.yahoo.com/"
+    }
+});
+
+var vue3 = new Vue({
+    el: "#vue3",
+    data: {
+        test: true,
+        activity: false,
+    }
+});
